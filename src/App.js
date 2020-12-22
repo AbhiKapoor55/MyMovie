1a4a49f8125b7d3e5c5ca4bd529003825a8df507
AbhiKapoor55 <abhishek.kapoor@mail.utoronto.ca>
2020-12-21 23:14:54 -0500

    added initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'; 
 import './App.css';
+import MovieCard from './components/MovieCard';
+import apiKey from './apiKey.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const originalMovies = [
+  {id: 1, title: 'Star Wars'}, 
+  {id: 2, title: 'Star Trek'}, 
+  {id: 3, title: 'Blade Runner'}
+];
+
+class App extends Component {
+  state = {movies: []}
+  async componentDidMount() {
+  // setTimeout(() => 
+  //   this.setState({movies: originalMovies}), 5000); 
+    const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`); 
+    const json = await response.json(); 
+    this.setState({movies: json.results})  
+  }
+  render () {
+    const {movies} = this.state; 
+
+    return (
+      <div className="App">
+        {movies.map(movie => <MovieCard key={movie.id} movie={movie}/>)}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/apiKey.js
@@ -0,0 +1,3 @@
+const apiKey = "e4750f73f4369c75e9c3e5617ad920d1"
+
+export default apiKey; 
